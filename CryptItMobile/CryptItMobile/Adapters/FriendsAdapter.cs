ca31298696d213ab293dc17ce60c1fbf91a71970
Dominiworld <dominiworld@gmail.com>
2016-04-06 00:12:01 +0700

    Main window load friends

---
+++ CryptItMobile/CryptItMobile/Adapters/FriendsAdapter.cs
@@ -0,0 +1,95 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.Graphics;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Java.Lang;
+using Model;
+using vkAPI;
+
+namespace CryptItMobile.Adapters
+{
+    public class FriendsAdapter: BaseAdapter
+    {
+        private LayoutInflater lInflater;
+        private UserService _userService = new UserService();
+        private List<User> _friends;
+
+       
+        public FriendsAdapter(Context context)
+        {
+            GetFriends();
+            
+            lInflater = (LayoutInflater)context.GetSystemService(Context.LayoutInflaterService);
+        }
+
+       
+        public override long GetItemId(int position)
+        {
+            return position;
+        }
+
+        public override View GetView(int position, View convertView, ViewGroup parent)
+        {
+            View view = lInflater.Inflate(Resource.Layout.Friend, null, false); ;
+
+            var imageBitmap = GetImageBitmapFromUrl(_friends[position].PhotoUrl);
+            view.FindViewById<ImageView>(Resource.Id.friendImageView).SetImageBitmap(imageBitmap);
+
+
+            view.FindViewById<TextView>(Resource.Id.friendTextView).Text = _friends[position].FullName;
+            //todo Сделать точку для друзей в онлайне
+            return view;
+        }
+
+        public override Java.Lang.Object GetItem(int position)
+        {
+            return null;
+        }
+
+        public override int Count
+        {
+            get
+            {
+                if (_friends!=null)
+                {
+                    return _friends.Count();
+                }
+                
+                return 0;
+            }
+        }
+
+        private async void GetFriends()
+        {
+            _friends = (await _userService.GetFriends(AuthorizeService.Instance.CurrentUserId)).ToList();
+            
+            _friends = _friends.OrderBy(f => f.LastName).ToList();
+            NotifyDataSetChanged();
+        }
+
+        private Bitmap GetImageBitmapFromUrl(string url)//todo Сделать async, перенести в отдельный класс
+        {
+            Bitmap imageBitmap = null;
+
+            using (var webClient = new WebClient())
+            {
+                var imageBytes = webClient.DownloadData(url);
+                if (imageBytes != null && imageBytes.Length > 0)
+                {
+                    imageBitmap = BitmapFactory.DecodeByteArray(imageBytes, 0, imageBytes.Length);
+                }
+            }
+
+            return imageBitmap;
+        }
+    }
+}
--- CryptItMobile/CryptItMobile/MainActivity.cs
+++ CryptItMobile/CryptItMobile/MainActivity.cs
@@ -1,17 +1,24 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using Android.App;
 using Android.Content;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using CryptItMobile.Adapters;
 using vkAPI;
+using Model;
 
 namespace CryptItMobile
 {
     [Activity(Label = "CryptItMobile")]
     public class MainActivity : Activity
     {
+        private ListView _friendsListView;
+        private FriendsAdapter _friendsAdapter;
+        
 
         protected override void OnCreate(Bundle bundle)
         {
@@ -19,8 +26,12 @@ namespace CryptItMobile
 
             SetContentView(Resource.Layout.Main);
             
-
+            _friendsListView = FindViewById<ListView>(Resource.Id.friendsListView);
+            _friendsAdapter = new FriendsAdapter(this);
+            _friendsListView.Adapter = _friendsAdapter;
         }
+
+        
     }
 }
 
