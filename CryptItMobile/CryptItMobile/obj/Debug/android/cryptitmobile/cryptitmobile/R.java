0370d454995ef19f8bd64438880dffd8e9327287
Dominiworld <dominiworld@gmail.com>
2016-04-15 23:10:39 +0700

    NOthing

--- CryptItMobile/CryptItMobile/obj/Debug/android/cryptitmobile/cryptitmobile/R.java
+++ CryptItMobile/CryptItMobile/obj/Debug/android/cryptitmobile/cryptitmobile/R.java
@@ -12,21 +12,31 @@ public final class R {
     }
     public static final class drawable {
         public static final int icon=0x7f020000;
+        public static final int online=0x7f020001;
+        public static final int rectangle_rounded_all=0x7f020002;
+        public static final int rectangle_rounded_bottom=0x7f020003;
+        public static final int rectangle_rounded_top=0x7f020004;
     }
     public static final class id {
-        public static final int dialogListView=0x7f050002;
-        public static final int enterButton=0x7f050001;
-        public static final int friendImageView=0x7f050003;
-        public static final int friendMessageTextView=0x7f050007;
-        public static final int friendMessageTimeTextView=0x7f050006;
-        public static final int friendTextView=0x7f050004;
-        public static final int friendsListView=0x7f050009;
-        public static final int messageEditText=0x7f050000;
-        public static final int myMessageTextView=0x7f05000b;
-        public static final int myMessageTimeTextView=0x7f05000a;
-        public static final int onlineImageView=0x7f050005;
-        public static final int searchEditText=0x7f050008;
-        public static final int webView=0x7f05000c;
+        public static final int attachFileButton=0x7f050003;
+        public static final int criptButton=0x7f050004;
+        public static final int dialogListView=0x7f050006;
+        public static final int enterButton=0x7f050005;
+        public static final int exitDialogButton=0x7f050001;
+        public static final int exitMainButton=0x7f05000d;
+        public static final int friendImageView=0x7f050008;
+        public static final int friendMessageTextView=0x7f05000c;
+        public static final int friendMessageTimeTextView=0x7f05000b;
+        public static final int friendTextView=0x7f050007;
+        public static final int friendsDialogButton=0x7f050000;
+        public static final int friendsListView=0x7f05000f;
+        public static final int messageEditText=0x7f050002;
+        public static final int myMessageTextView=0x7f050011;
+        public static final int myMessageTimeTextView=0x7f050010;
+        public static final int newMassageTextView=0x7f050009;
+        public static final int onlineImageView=0x7f05000a;
+        public static final int searchEditText=0x7f05000e;
+        public static final int webView=0x7f050012;
     }
     public static final class layout {
         public static final int dialog=0x7f030000;
